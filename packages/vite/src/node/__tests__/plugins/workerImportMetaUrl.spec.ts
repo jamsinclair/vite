af0f76101896bc9cecc49801619349293e27590a
Jamie <5964236+jamsinclair@users.noreply.github.com>
2024-12-26 10:45:51 +0000

    confirgure root too

--- packages/vite/src/node/__tests__/plugins/workerImportMetaUrl.spec.ts
+++ packages/vite/src/node/__tests__/plugins/workerImportMetaUrl.spec.ts
@@ -8,11 +8,7 @@ import { PartialEnvironment } from '../../baseEnvironment'
 async function createWorkerImportMetaUrlPluginTransform() {
   const root = path.posix.join(import.meta.dirname, 'fixtures/worker')
   console.log({ root })
-  const config = await resolveConfig({ configFile: false }, 'serve')
-
-  console.log({
-    configRoot: config.root
-  })
+  const config = await resolveConfig({ configFile: false, root }, 'serve')
   const instance = workerImportMetaUrlPlugin(config)
   const environment = new PartialEnvironment('client', config)
 
